4c2b92edbca80ce346bf5b6e5655f81fbc45f9bd
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2024-10-11 20:15:20 +0200

    feat(elements): create header component

---
+++ src/app/shared/layout/header/header.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    MenubarModule
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeaderComponent {
+  items: MenuItem[] = [];
+}
