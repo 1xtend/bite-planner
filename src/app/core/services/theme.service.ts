160de75acffd3158399e9eeae26dfd7a853049c8
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2024-10-12 00:00:11 +0200

    feat(common): provide two themes, create theme service, localStorage enum

---
+++ src/app/core/services/theme.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Theme } from '../../shared/models/types/theme.type';
+import { LocalStorage } from '../../shared/models/enums/local-storage.enum';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+  private themeSubject = new Subject<Theme>();
+
+  constructor() {
+  }
+
+  setTheme(theme: Theme): void {
+    const linkEl = document.getElementById('app-theme') as HTMLLinkElement | null;
+    if (!linkEl) {
+      throw new Error('There is no "app-theme" element on the page.');
+    }
+
+    linkEl.href = `lara-${ theme }-green.css`;
+    localStorage.setItem(LocalStorage.Theme, theme);
+    this.themeSubject.next(theme);
+  }
+}
