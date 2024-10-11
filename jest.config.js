fbe6a43242fe0d113c789d3f20d404c45b8d9e30
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2024-10-11 19:09:44 +0200

    feat(core): setup Jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+  preset: 'jest-preset-angular',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!flat)/'
+  ],
+  moduleDirectories: ['node_modules', 'src'],
+  fakeTimers: {
+    enableGlobally: true
+  }
+};
