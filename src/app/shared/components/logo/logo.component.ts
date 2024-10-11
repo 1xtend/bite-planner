f2dd325ad4b07b07e20cb08c52bb33356a245152
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2024-10-11 22:40:13 +0200

    feat(elements): display header, create icon-button, logo and theme-switch components

---
+++ src/app/shared/components/logo/logo.component.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-logo',
+  standalone: true,
+  imports: [
+    ButtonModule,
+    RouterLink
+  ],
+  templateUrl: './logo.component.html',
+  styleUrl: './logo.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LogoComponent {
+
+}
