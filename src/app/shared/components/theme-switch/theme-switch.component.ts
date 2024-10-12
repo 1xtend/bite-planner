ce7147490f84adbaf6791b884370cdbcb2023ba8
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2024-10-12 12:08:06 +0200

    feat(common): change theme-switch icon depend on theme, show tooltip on hover

--- src/app/shared/components/theme-switch/theme-switch.component.ts
+++ src/app/shared/components/theme-switch/theme-switch.component.ts
@@ -1,13 +1,17 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { IconButtonComponent } from '../icon-button/icon-button.component';
 import { ThemeService } from '../../../core/services/theme.service';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { TooltipModule } from 'primeng/tooltip';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-theme-switch',
   standalone: true,
   imports: [
-    IconButtonComponent
+    IconButtonComponent,
+    TooltipModule,
+    TranslateModule
   ],
   templateUrl: './theme-switch.component.html',
   styleUrl: './theme-switch.component.scss',
@@ -16,6 +20,8 @@ import { toSignal } from '@angular/core/rxjs-interop';
 export class ThemeSwitchComponent {
   private themeService = inject(ThemeService);
   currentTheme = toSignal(this.themeService.theme$);
+  icon = computed<string>(() => this.currentTheme() === 'light' ? 'pi pi-moon' : 'pi pi-sun');
+  tooltip = computed<string>(() => `tooltip.switch-${ this.currentTheme() }-theme`);
 
   toggle(): void {
     const theme = this.currentTheme() === 'dark' ? 'light' : 'dark';
