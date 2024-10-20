392e15a5ecbdfbcd5db44f49524c1bf1ddbe99a2
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2024-10-20 13:03:16 +0200

    test: display-control-error component

--- src/app/shared/components/display-control-error/display-control-error.component.spec.ts
+++ src/app/shared/components/display-control-error/display-control-error.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DisplayControlErrorComponent } from './display-control-error.component';
+import { FormControl } from '@angular/forms';
 
 describe('DisplayControlErrorComponent', () => {
   let component: DisplayControlErrorComponent;
@@ -10,14 +11,22 @@ describe('DisplayControlErrorComponent', () => {
     await TestBed.configureTestingModule({
       imports: [DisplayControlErrorComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(DisplayControlErrorComponent);
     component = fixture.componentInstance;
+
+    const control = new FormControl('');
+    fixture.componentRef.setInput('control', control);
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have correct errors array', () => {
+    expect(component.errors).toEqual(['email', 'username', 'required']);
   });
 });
--- src/app/shared/components/display-control-error/display-control-error.component.ts
+++ src/app/shared/components/display-control-error/display-control-error.component.ts
@@ -5,9 +5,7 @@ import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'app-display-control-error',
   standalone: true,
-  imports: [
-    TranslateModule
-  ],
+  imports: [TranslateModule],
   templateUrl: './display-control-error.component.html',
   styleUrl: './display-control-error.component.scss'
 })
