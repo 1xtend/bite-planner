fadc01988712ff006848e815e9713a286cb21efc
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2024-10-20 15:27:10 +0200

    test: improve app component specs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,40 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { provideRouter } from '@angular/router';
+import { ThemeService } from './core/services/theme.service';
+import { mockLocaleService, mockThemeService } from './testing/mock-services';
+import { LocaleService } from './core/services/locale.service';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-      providers: [provideRouter([])]
+      providers: [
+        { provide: ThemeService, useValue: mockThemeService },
+        { provide: LocaleService, useValue: mockLocaleService }
+      ]
     }).compileComponents();
+
+    const fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  describe('when ngOnInit is called', () => {
+    it('should set saved theme', () => {
+      mockThemeService.getSavedTheme.mockReturnValue('dark');
+      component.ngOnInit();
+      expect(mockThemeService.setTheme).toHaveBeenCalledWith('dark');
+    });
+
+    it('should set saved locale', () => {
+      mockLocaleService.getSavedLanguage.mockReturnValue('en');
+      component.ngOnInit();
+      expect(mockLocaleService.setLanguage).toHaveBeenCalledWith('en');
+    });
   });
 });
--- src/app/testing/mock-services.ts
+++ src/app/testing/mock-services.ts
@@ -1,10 +1,12 @@
 import { BehaviorSubject, of } from 'rxjs';
 import { Theme } from '../shared/models/types/theme.type';
+import { Language } from '../shared/models/types/language.type';
 
 // ThemeService
 export const mockThemeService = {
   theme$: new BehaviorSubject<Theme>('light'),
-  setTheme: jest.fn()
+  setTheme: jest.fn(),
+  getSavedTheme: jest.fn()
 };
 
 // AuthService
@@ -17,3 +19,10 @@ export const mockFirebaseAuth = {};
 
 // ActivatedRoute
 export const mockActivatedRoute = {};
+
+// LocaleService
+export const mockLocaleService = {
+  language$: new BehaviorSubject<Language>('en'),
+  setLanguage: jest.fn(),
+  getSavedLanguage: jest.fn()
+};
