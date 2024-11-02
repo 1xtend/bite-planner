34024d6c14b973160c2014015df6022c6b9ef147
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2024-11-02 23:22:31 +0100

    test: breakpoint-observer service

--- src/app/core/services/auth.service.spec.ts
+++ src/app/core/services/auth.service.spec.ts
@@ -12,25 +12,9 @@ import { HttpErrorService } from './http-error.service';
 import { provideFirestore } from '@angular/fire/firestore';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../../../environments/environment';
-import { BehaviorSubject, first, of } from 'rxjs';
-import { SignupFormValue } from '../../shared/models/types/signup-form-value.type';
-
-// jest.mock('@angular/fire/auth', () => ({
-//   ...jest.requireActual('@angular/fire/auth'),
-//   authState: jest.fn(() => new BehaviorSubject({})),
-//   createUserWithEmailAndPassword: jest.fn(() => Promise.resolve({})),
-//   signInWithEmailAndPassword: jest.fn(),
-//   signOut: jest.fn()
-// }));
 
 describe('AuthService', () => {
   let service: AuthService;
-  let signupFormValue: SignupFormValue = {
-    email: 'test@gmail.com',
-    password: 'Password1',
-    username: 'test'
-  };
-  const user = { user: { uid: 'testUID', email: 'test@gmail.com' } };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -50,33 +34,5 @@ describe('AuthService', () => {
     expect(service).toBeTruthy();
   });
 
-  describe('when signup is called', () => {
-    afterEach(() => {
-      signupFormValue = {
-        email: 'test@gmail.com',
-        password: 'Password1',
-        username: 'test'
-      };
-    });
-
-    it('should throw error when username is not provided', () => {
-      signupFormValue = {
-        ...signupFormValue,
-        username: undefined as unknown as string
-      };
-      expect(() => service.signup(signupFormValue)).toThrow('You must provide username to signup!');
-    });
-
-    it('should return User after successful signup', () => {
-      const userCredential = { user } as unknown as auth.UserCredential;
-      const createSpy = jest.spyOn(auth, 'createUserWithEmailAndPassword')
-      // jest.mocked(createUserWithEmailAndPassword).mockReturnValue(Promise.resolve(userCredential));
-      mockHttpErrorService.handleError.mockReturnValue(of(user));
-
-      service.signup(signupFormValue).pipe(first()).subscribe((user) => {
-        expect(createSpy).toHaveBeenCalledWith(mockAuth, signupFormValue.email, signupFormValue.password);
-        // expect(user).toBeTruthy();
-      });
-    });
-  });
+  // TODO: Test auth
 });
--- src/app/core/services/breakpoint-observer.service.spec.ts
+++ src/app/core/services/breakpoint-observer.service.spec.ts
@@ -1,9 +1,36 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, flush, TestBed } from '@angular/core/testing';
 
 import { BreakpointObserver } from './breakpoint-observer.service';
+import { Breakpoint } from '../../shared/models/types/breakpoint.type';
+import { take } from 'rxjs';
 
 describe('BreakpointObserverService', () => {
   let service: BreakpointObserver;
+  const breakpoints: { [K in Breakpoint]: number } = {
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1200
+  };
+
+  function setWindowWidth(width: number): void {
+    Object.assign(window, {
+      ...window,
+      innerWidth: width
+    });
+  }
+
+  function testBreakpoint(expectedValue: Breakpoint, width: number, breakpoints?: Breakpoint | Breakpoint[]): void {
+    setWindowWidth(width);
+
+    service.observe(breakpoints).pipe(take(1)).subscribe((value) => {
+      expect(value).toBe(expectedValue);
+    });
+
+    window.dispatchEvent(new Event('resize'));
+
+    flush();
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -12,5 +39,32 @@ describe('BreakpointObserverService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return breakpoints when getBreakpoints is called', () => {
+    expect(service.getBreakpoints()).toEqual(breakpoints);
+  });
+
+  describe('when observe is called', () => {
+    it('should emit sm breakpoint on window resize', fakeAsync(() => {
+      testBreakpoint('sm', 500);
+    }));
+
+    it('should emit md breakpoint on window resize', fakeAsync(() => {
+      testBreakpoint('md', 800);
+    }));
+
+    it('should emit lg breakpoint on window resize', fakeAsync(() => {
+      testBreakpoint('lg', 1000);
+    }));
+
+    it('should emit xl breakpoint on window resize', fakeAsync(() => {
+      testBreakpoint('xl', 1300);
+    }));
+
+    it('should emit only provided breakpoints', fakeAsync(() => {
+      testBreakpoint('lg', 1000, 'lg');
+      testBreakpoint('sm', 400, ['sm', 'md']);
+    }));
   });
 });
